5afc3ed6e383fa6b174b8615e3a43610da641ade
ConanJian <conanjian2@gmail.com>
2022-04-08 02:26:00 -0700

    Add project files.

---
+++ CallToDoApi/ApiClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace CallToDoApi
+{
+    public class ApiClient
+    {
+        private readonly HttpClient _client;
+        public ApiClient(string uri, string defaultRequestHeader)
+        { 
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri(uri);
+        }
+    }
+}
---
+++ ToDoConsole/ToDoConsole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDoConsole
+{
+    internal class ToDoConsole
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
