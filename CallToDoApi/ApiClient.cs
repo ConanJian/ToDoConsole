60dd3bfe4ebc2bc4b61b961ec03aabc3d1e54ddb
ConanJian <conanjian2@gmail.com>
2022-04-08 02:37:54 -0700

    Added IDisposable to ApiClient

--- CallToDoApi/ApiClient.cs
+++ CallToDoApi/ApiClient.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace CallToDoApi
 {
-    public class ApiClient
+    public class ApiClient: IDisposable
     {
         private readonly HttpClient _client;
-        public ApiClient(string uri, string defaultRequestHeader)
+        public ApiClient(string uri, string defaultHeader)
         { 
             _client = new HttpClient();
             _client.BaseAddress = new Uri(uri);
+            //figure out how to get defaultHeader to work without having to give
+            //people instructions
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
         }
     }
 }
