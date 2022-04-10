c8ae848644d17d091275d1eaafedc135bccbb666
ConanJian <conanjian2@gmail.com>
2022-04-10 00:29:29 -0700

    Created a terminal that is responsive to user input and can get todo list information by calling Apis

--- ToDoConsole/ToDoConsole.cs
+++ ToDoConsole/ToDoConsole.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using ToDoTerminal;
 
 namespace ToDoConsole
 {
     internal class ToDoConsole
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (Terminal terminal = new Terminal())
+            {
+                Console.WriteLine("Welcome to the Terminal!\n" +
+                    "Your Current ToDo List is:");
+                await terminal.PrintCompleteToDoList();
+
+                bool running = true;
+                while (running)
+                {
+                    try
+                    {
+                        Console.WriteLine("\nCommand: ");
+                        string command = Console.ReadLine().ToLower();
+
+                        switch (command)
+                        {
+                            case "list":
+                                await terminal.PrintCompleteToDoList();
+                                break;
+                            case "priority list":
+                                await terminal.PrintPriorityToDoList();
+                                break;
+                            case "quit":
+                                running = false;
+                                break;
+                            default:
+                                Console.WriteLine("Invalid Command. Type 'quit' to quit");
+                                break;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Error Message: {e.Message}");
+                    }
+                }
+            }
         }
     }
 }
---
+++ ToDoTerminal/Terminal.cs
@@ -0,0 +1,71 @@
+﻿using CallApi;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ToDoTerminal
+{
+    public class Terminal: IDisposable
+    {
+        private readonly ToDoApiClient _client;
+        private List<ToDoModel> toDoListCache;
+        public Terminal()
+        {
+            _client = new ToDoApiClient();
+            toDoListCache = _client.GetCompleteToDoListAsync().Result;
+        }
+        public async Task PrintCompleteToDoList()
+        {
+            toDoListCache = await _client.GetCompleteToDoListAsync();
+            PrintToDoList(toDoListCache);
+        }
+        public async Task PrintPriorityToDoList()
+        {
+            Console.Write($"ToDoList by Priority\n" +
+                $"Options: (1, 2, 3)\n" +
+                $"Priority: ");
+
+            bool notCompleted = true;
+            int priority = -1;
+            while (notCompleted)
+            {
+                try
+                {
+                    //Reads in input and validates if it is valid
+                    string priorityInput = Console.ReadLine();
+                    bool isInteger = int.TryParse(priorityInput, out priority);
+                    if (!isInteger)
+                    {
+                        throw new Exception("priorityInput not an integer");
+                    }
+                    if (priority > 3 || priority < 1)
+                    {
+                        throw new Exception("priorityInput not 1, 2, or 3");
+                    }
+                    notCompleted = false;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Incorrect Input try again: ");
+                }
+            }
+
+            var toDoList = await _client.GetPriorityToDoListAsync((Priority)priority);
+            PrintToDoList(toDoList);
+        }
+        private void PrintToDoList(List<ToDoModel> toDoList)
+        {
+            for (int i = 0; i < toDoList.Count; i++)
+            {
+                ToDoModel toDoItem = toDoList[i];
+                string output = $"  {i + 1}) {toDoItem.Message}" +
+                    $"\nPriority: {(Priority)toDoItem.Priority}";
+                Console.WriteLine(output);
+            }
+        }
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+    }
+}
